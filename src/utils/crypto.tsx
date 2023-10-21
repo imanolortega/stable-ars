41eb303fc32240a955a5216b8da43fec111c3117
imanol <imanolrtega@gmail.com>
2023-10-21 13:05:50 -0300

    Fix revalidate

--- src/utils/crypto.tsx
+++ src/utils/crypto.tsx
@@ -15,14 +15,20 @@ export async function getCryptoCurrencies() {
 
   const stableCoins = ["dai", "usdc", "usdt"];
 
-  const allStableCoins: Array<{ exchange: string, name: string, data: Record<string, any> }> = [];
+  const allStableCoins: Array<{
+    exchange: string;
+    name: string;
+    data: Record<string, any>;
+  }> = [];
 
   for (const exchange of exchanges) {
     const exchangeData: Record<string, any> = {};
 
     for (const coin of stableCoins) {
       const url = `https://criptoya.com/api/${exchange}/${coin}/ars/0.1`;
-      const response = await fetch(url);
+      const response = await fetch(url, {
+        next: { revalidate: 60 },
+      });
       const data = await response.json();
       exchangeData[coin] = data;
     }
