0a8f16e6f9904ff1d6f8c65eb640103c1c3b71ef
imanol <imanolrtega@gmail.com>
2023-10-20 23:58:00 -0300

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,37 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from "./page.module.css";
+import { getCryptoCurrencies } from "@/utils/crypto";
 
-export default function Home() {
+export default async function Home() {
+  const data = await getCryptoCurrencies();
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {data.map((exchange) => (
+        <section key={exchange.name}>
+          <div className={styles.header}>
+            <h2>{exchange.name}</h2>
+          </div>
+          <div className={styles.center}>
+            <div className={styles.grid}>
+              <div className={styles.card}>
+                <h2>DAI</h2>
+                <p>Compra: ${parseFloat(exchange.data.dai.ask).toFixed(2)}</p>
+                <p>Venta: ${parseFloat(exchange.data.dai.bid).toFixed(2)}</p>
+              </div>
+              <div className={styles.card}>
+                <h2>USDC</h2>
+                <p>Compra: ${parseFloat(exchange.data.usdc.ask).toFixed(2)}</p>
+                <p>Venta: ${parseFloat(exchange.data.usdc.bid).toFixed(2)}</p>
+              </div>
+              <div className={styles.card}>
+                <h2>USDT</h2>
+                <p>Compra: ${parseFloat(exchange.data.usdt.ask).toFixed(2)}</p>
+                <p>Venta: ${parseFloat(exchange.data.usdt.bid).toFixed(2)}</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      ))}
     </main>
-  )
+  );
 }
---
+++ src/utils/crypto.tsx
@@ -0,0 +1,36 @@
+export async function getCryptoCurrencies() {
+  const exchangeNameMapping: Record<string, string> = {
+    belo: "Belo",
+    buenbit: "BuenBit",
+    bybit: "ByBit",
+    fiwind: "FiWind",
+    lemoncash: "Lemon Cash",
+    letsbit: "LetsBit",
+    ripio: "Ripio",
+    satoshitango: "Satoshi Tango",
+    tiendacrypto: "Tienda Crypto",
+  };
+
+  const exchanges = Object.keys(exchangeNameMapping);
+
+  const stableCoins = ["dai", "usdc", "usdt"];
+
+  const allStableCoins: Array<{ exchange: string, name: string, data: Record<string, any> }> = [];
+
+  for (const exchange of exchanges) {
+    const exchangeData: Record<string, any> = {};
+
+    for (const coin of stableCoins) {
+      const url = `https://criptoya.com/api/${exchange}/${coin}/ars/0.1`;
+      const response = await fetch(url);
+      const data = await response.json();
+      exchangeData[coin] = data;
+    }
+
+    const name = exchangeNameMapping[exchange];
+
+    allStableCoins.push({ exchange, name, data: exchangeData });
+  }
+
+  return allStableCoins;
+}
