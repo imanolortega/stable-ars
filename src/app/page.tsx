8b0cab424ccb01cd1b69ec054fa6449c20b150d8
imanol <imanolrtega@gmail.com>
2023-10-21 02:35:21 -0300

    Add spread

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,16 +17,34 @@ export default async function Home() {
                 <h3>DAI</h3>
                 <p>Compra: ${parseFloat(exchange.data.dai.ask).toFixed(2)}</p>
                 <p>Venta: ${parseFloat(exchange.data.dai.bid).toFixed(2)}</p>
+                <p>
+                  Spread:{" "}$
+                  {(exchange.data.dai.ask - exchange.data.dai.bid)
+                    .toFixed(2)
+                    .toString()}
+                </p>
               </div>
               <div className={styles.card}>
                 <h3>USDC</h3>
                 <p>Compra: ${parseFloat(exchange.data.usdc.ask).toFixed(2)}</p>
                 <p>Venta: ${parseFloat(exchange.data.usdc.bid).toFixed(2)}</p>
+                <p>
+                  Spread:{" "}$
+                  {(exchange.data.usdc.ask - exchange.data.usdc.bid)
+                    .toFixed(2)
+                    .toString()}
+                </p>
               </div>
               <div className={styles.card}>
                 <h3>USDT</h3>
                 <p>Compra: ${parseFloat(exchange.data.usdt.ask).toFixed(2)}</p>
                 <p>Venta: ${parseFloat(exchange.data.usdt.bid).toFixed(2)}</p>
+                <p>
+                  Spread:{" "}$
+                  {(exchange.data.usdt.ask - exchange.data.usdt.bid)
+                    .toFixed(2)
+                    .toString()}
+                </p>
               </div>
             </div>
           </div>
