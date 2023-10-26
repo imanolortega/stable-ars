d8bb49240bc9b6037a9d2f252dd01828beb4fe2e
imanol <imanolrtega@gmail.com>
2023-10-25 22:25:52 -0300

    Add Best Crypto Card component

---
+++ src/app/crypto-card/best-crypto-card.tsx
@@ -0,0 +1,43 @@
+import styles from "../page.module.css";
+import { Dai, Usdc, Usdt } from "./icons/icons";
+
+interface BestCryptoCardProps {
+  ask: { exchange: string; value: number } | null;
+  bid: { exchange: string; value: number } | null;
+  currency: string;
+  spread: { exchange: string; value: number } | null;
+}
+
+const BestCryptoCard: React.FC<BestCryptoCardProps> = ({
+  ask,
+  bid,
+  currency,
+  spread }) => {
+  const currencyIcons: Record<string, any> = {
+    dai: <Dai />,
+    usdc: <Usdc />,
+    usdt: <Usdt />,
+  };
+
+  return (
+    <div className={styles.card}>
+      <div className={styles["card-header"]}>
+        <div>
+          {currencyIcons[currency]}
+        </div>
+        <h3>{currency.toUpperCase()}</h3>
+      </div>
+      <p>
+        Comprar: {ask?.exchange} a ${ask?.value}{" "}
+      </p>
+      <p>
+        Vender: {bid?.exchange} a ${bid?.value}
+      </p>
+      <p>
+        Spread: {spread?.exchange} a ${spread?.value}
+      </p>
+    </div>
+  );
+};
+
+export default BestCryptoCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,11 @@ import {
   getCryptoCurrencies,
 } from "@/utils/crypto";
 import CryptoCard from "./crypto-card/crypto-card";
+import BestCryptoCard from "./crypto-card/best-crypto-card";
 
 export default async function Home() {
   const data = await getCryptoCurrencies();
-  const bestDAIAsk = findBestAskPrice(data, "dai");
-  const bestUSDCAsk = findBestAskPrice(data, "usdc");
-  const bestUSDTAsk = findBestAskPrice(data, "usdt");
-
-  const bestDAIBid = findBestBidPrice(data, "dai");
-  const bestUSDCBid = findBestBidPrice(data, "usdc");
-  const bestUSDTBid = findBestBidPrice(data, "usdt");
-
-  const lowestDAISpread = findLowestSpread(data, "dai");
-  const lowestUSCDSpread = findLowestSpread(data, "usdc");
-  const lowestUSDTSpread = findLowestSpread(data, "usdt");
+  const currencies = ["dai", "usdc", "usdt"]
 
   return (
     <main className={styles.main}>
@@ -30,44 +21,17 @@ export default async function Home() {
         </div>
         <div className={styles.center}>
           <div className={styles.grid}>
-            <div className={styles.card}>
-              <h3>DAI</h3>
-              <p>
-                Comprar: {bestDAIAsk?.exchange} a ${bestDAIAsk?.value}{" "}
-              </p>
-              <p>
-                Vender: {bestDAIBid?.exchange} a ${bestDAIBid?.value}
-              </p>
-              <p>
-                Spread: {lowestDAISpread?.exchange} a ${lowestDAISpread?.value}
-              </p>
-            </div>
-            <div className={styles.card}>
-              <h3>USDC</h3>
-              <p>
-                Comprar: {bestUSDCAsk?.exchange} a ${bestUSDCAsk?.value}{" "}
-              </p>
-              <p>
-                Vender: {bestUSDCBid?.exchange} a ${bestUSDCBid?.value}
-              </p>
-              <p>
-                Spread: {lowestUSCDSpread?.exchange} a $
-                {lowestUSCDSpread?.value}
-              </p>
-            </div>
-            <div className={styles.card}>
-              <h3>USDT</h3>
-              <p>
-                Comprar: {bestUSDTAsk?.exchange} a ${bestUSDTAsk?.value}{" "}
-              </p>
-              <p>
-                Vender: {bestUSDTBid?.exchange} a ${bestUSDTBid?.value}
-              </p>
-              <p>
-                Spread: {lowestUSDTSpread?.exchange} a $
-                {lowestUSDTSpread?.value}
-              </p>
-            </div>
+            {
+              currencies.map((currency) => (
+                <BestCryptoCard
+                  key={currency}
+                  currency={currency}
+                  ask={findBestAskPrice(data, currency)}
+                  bid={findBestBidPrice(data, currency)}
+                  spread={findLowestSpread(data, currency)}
+                />
+              ))
+            }
           </div>
         </div>
       </section>
