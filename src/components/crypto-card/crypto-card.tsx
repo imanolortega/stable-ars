13516cd8baa72e0f25359688815c26c6b10b16cd
imanol <imanolrtega@gmail.com>
2023-11-08 15:57:47 -0300

    Change cards color

--- src/components/crypto-card/crypto-card.tsx
+++ src/components/crypto-card/crypto-card.tsx
@@ -10,13 +10,13 @@ interface CryptoCardProps {
 
 const colorMap: any = {
   dai: {
-    primaryGlowColor: "conic-gradient(from 180deg at 50% 50%, #ffff0033 0deg, #ffcc001d 55deg, #d4ff0028 120deg, #99ff0029 160deg, transparent 360deg)",
+    primaryGlowColor: "conic-gradient(from 180deg at 50% 50%, #ffff0044 0deg, #ffcc0033 55deg, #d4ff0040 120deg, #99ff0044 160deg, transparent 360deg)",
   },
   usdc: {
-    primaryGlowColor: "conic-gradient(from 180deg at 50% 50%, #16abff33 0deg, #0885ff33 55deg, #54d6ff33 120deg, #0071ff33 160deg, transparent 360deg)",
+    primaryGlowColor: "conic-gradient(from 180deg at 50% 50%, #16abff55 0deg, #0885ff55 55deg, #54d6ff55 120deg, #0071ff55 160deg, transparent 360deg)",
   },
   usdt: {
-    primaryGlowColor: "conic-gradient(from 180deg at 50% 50%, #00ff0033 0deg, #00cc003d 55deg, #16ea162b 120deg, #00990033 160deg, transparent 360deg)",
+    primaryGlowColor: "conic-gradient(from 180deg at 50% 50%, #00ff0044 0deg, #00cc0044 55deg, #16ea162b 120deg, #00990044 160deg, transparent 360deg)",
   },
 };
 
