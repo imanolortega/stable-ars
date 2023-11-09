6350591ad62983a279b06b72c101a41238df8dde
imanol <imanolrtega@gmail.com>
2023-11-09 01:41:28 -0300

    Change timezone

--- src/utils/common.tsx
+++ src/utils/common.tsx
@@ -1,4 +1,4 @@
-const moment = require('moment');
+const moment = require('moment-timezone');
 
 function capitalizeFirstLetter(text: string) {
   return text.charAt(0).toUpperCase() + text.slice(1);
