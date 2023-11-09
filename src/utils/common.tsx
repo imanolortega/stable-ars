276011c627351c7e21cfa0f1ad8edbfc352b19c9
imanol <imanolrtega@gmail.com>
2023-11-09 01:56:42 -0300

    Fix timezone

--- src/utils/common.tsx
+++ src/utils/common.tsx
@@ -1,22 +1,24 @@
-const moment = require('moment-timezone');
+import moment from "moment-timezone";
 
 function capitalizeFirstLetter(text: string) {
   return text.charAt(0).toUpperCase() + text.slice(1);
 }
 
 export function formatTimestampToDateTime(timestamp: number) {
-  const formattedDateTime = moment.unix(timestamp).locale('es-ES').format('DD/MM/YY HH:MM');
-  return formattedDateTime;
+  const date = moment
+    .tz(moment.tz(timestamp.toString()), "America/Argentina/Buenos_Aires")
+    .format("DD/MM/YY HH:MM");
+  return date;
 }
 
 export function formatDateToSpanish(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-  }
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-  const formatter = new Intl.DateTimeFormat('es-ES', options)
-  const capitalizeDate = capitalizeFirstLetter(formatter.format(date))
-  return capitalizeDate
+  const formatter = new Intl.DateTimeFormat("es-ES", options);
+  const capitalizeDate = capitalizeFirstLetter(formatter.format(date));
+  return capitalizeDate;
 }
