cdb743dc4eaca002d7b938feefd9c78b85f3717e
imanol <imanolrtega@gmail.com>
2023-11-10 01:26:43 -0300

    Change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Providers } from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Stablecoins en Argentina",
+  title: "Stablears | Stablecoins en Argentina",
   description:
     "Precios de stablecoins en Argentina: DAI, USDC, USDT en pesos argentinos.",
 };
@@ -19,10 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="dark" style={{ colorScheme: "dark" }}>
-      <link
-        rel="icon"
-        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🇦🇷</text></svg>"
-      />
+      <link rel="icon" href="/favicon.ico" />
       <body className={inter.className}>
         <Providers>
           {children} <Analytics />
