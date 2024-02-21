635863080f00372a2ef220ab931604e31aed0baa
imanol <imanolrtega@gmail.com>
2024-02-20 21:12:00 -0300

    Fix select style in mobile

---
+++ src/utils/info.tsx
@@ -0,0 +1,76 @@
+export const DAI = (
+  <>
+    DAI es una criptomoneda estable descentralizada que está vinculada al valor
+    del dólar estadounidense. Fue desarrollada por MakerDAO, una organización
+    descentralizada que opera en la plataforma Ethereum. La estabilidad de DAI
+    se mantiene a través de un sistema de contratos inteligentes y activos
+    colaterales dentro de la red Ethereum.
+    <br />
+    El proceso funciona de la siguiente manera: los usuarios bloquean activos
+    como Ethereum en un contrato inteligente de MakerDAO y reciben DAI a cambio.
+    Estos activos bloqueados actúan como garantía, y si el valor de DAI cae por
+    debajo de 1 dólar, el sistema puede liquidar automáticamente parte de los
+    activos bloqueados para mantener la estabilidad del valor de DAI.
+    <br />
+    DAI se utiliza principalmente como una forma de estabilidad dentro del
+    ecosistema de las criptomonedas, permitiendo a los usuarios mantener valor
+    sin estar expuestos a la volatilidad extrema que a menudo se asocia con
+    otras criptomonedas como Bitcoin o Ethereum. Esto hace que DAI sea útil para
+    actividades como el comercio, préstamos, remesas y más, donde la estabilidad
+    de precios es crucial.
+  </>
+);
+
+export const USDC = (
+  <>
+    USDC, que significa &quot;USD Coin&quot;, es otra criptomoneda estable
+    vinculada al dólar estadounidense. Al igual que DAI, USDC está diseñada para
+    mantener un valor estable en relación con el dólar estadounidense, pero
+    difiere en algunos aspectos fundamentales.
+    <br />
+    USDC fue creado a través de una colaboración entre Circle y Coinbase, dos
+    empresas prominentes en el espacio de las criptomonedas. A diferencia de
+    DAI, que se mantiene mediante un sistema descentralizado de contratos
+    inteligentes y colaterales en la red Ethereum, USDC se emite y se respalda
+    por reservas de dólares estadounidenses mantenidas en cuentas bancarias
+    reguladas y auditadas por terceros. Esto significa que la emisión y la
+    circulación de USDC están más centralizadas en comparación con DAI.
+    <br />A pesar de estas diferencias en el enfoque técnico, tanto DAI como
+    USDC se utilizan ampliamente en el espacio de las criptomonedas para
+    proporcionar estabilidad de precios y facilitar transacciones y
+    transferencias de valor sin la volatilidad asociada con otras criptomonedas
+    más tradicionales como Bitcoin o Ethereum. Los usuarios pueden utilizar USDC
+    para una variedad de propósitos, incluidos pagos, comercio, préstamos y más,
+    aprovechando la estabilidad del dólar estadounidense sin tener que recurrir
+    a sistemas financieros tradicionales.
+  </>
+);
+
+export const USDT = (
+  <>
+    USDT, también conocida como "Tether", es otra criptomoneda estable vinculada
+    al dólar estadounidense. Al igual que DAI y USDC, USDT está diseñado para
+    mantener un valor estable en relación con el dólar estadounidense.
+    <br />
+    Sin embargo, a diferencia de DAI y USDC, que tienen diferentes estructuras y
+    enfoques para mantener su estabilidad, USDT se diferencia en su método de
+    respaldo. USDT fue el primero en introducir el concepto de criptomoneda
+    estable respaldada por activos, y originalmente afirmaba estar respaldada
+    por una cantidad equivalente de dólares estadounidenses en una cuenta
+    bancaria. Sin embargo, la transparencia y la verificación de estas reservas
+    han sido objeto de debate y controversia en el pasado.
+    <br />
+    A pesar de esto, USDT se ha convertido en una de las criptomonedas estables
+    más populares y ampliamente utilizadas en el mercado de las criptomonedas.
+    Se utiliza en una variedad de casos de uso, incluyendo el comercio de
+    criptomonedas en exchanges, como una forma de mantener el valor durante
+    períodos de volatilidad del mercado, y para facilitar transferencias de
+    valor rápidas y eficientes entre diferentes exchanges y billeteras
+    digitales.
+    <br />
+    Es importante tener en cuenta que, debido a la controversia sobre la
+    transparencia de las reservas de USDT, algunos usuarios y empresas prefieren
+    utilizar otras criptomonedas estables como DAI o USDC, que ofrecen un mayor
+    nivel de transparencia y descentralización en sus estructuras.
+  </>
+);
