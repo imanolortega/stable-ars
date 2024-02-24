f1a5dbe45c70a4b23a1682e61f601eb1831dc1af
imanol <imanolrtega@gmail.com>
2024-02-24 20:32:18 -0300

    Add breadcrumb

--- src/app/sobre-stablecoins/page.tsx
+++ src/app/sobre-stablecoins/page.tsx
@@ -1,12 +1,24 @@
 import { ABOUT, cryptoInfo } from "@/utils/info";
 import Aside from "@/components/aside/aside";
 import styles from "./page.module.scss";
+import Link from "next/link";
 
 export default async function Home() {
   return (
     <div className={styles["article-page"]}>
       <Aside />
       <article className={styles["article"]}>
+        <div className={styles["breadcrumb"]}>
+          <nav aria-label="breadcrumb" role="navigation">
+            <ol>
+              <li>
+                <Link href="/">Inicio</Link>
+              </li>
+              <li><span>{">"}</span></li>
+              <li><span>Sobre Stablecoins</span></li>
+            </ol>
+          </nav>
+        </div>
         <div className={styles["header"]}>
           <h1>¿Qué son las Stablecoins?</h1>
         </div>
