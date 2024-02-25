1a7221df6da79435084306fb6fd9de31e04d2549
imanol <imanolrtega@gmail.com>
2024-02-25 17:51:46 -0300

    Fix overflow in mobile menu

--- src/components/header/menu-mobile/menu-mobile.tsx
+++ src/components/header/menu-mobile/menu-mobile.tsx
@@ -16,9 +16,9 @@ export default function MenuMobile() {
 
   useEffect(() => {
     if (menuOpen) {
-      document.body.style.overflow = "hidden";
+      document.documentElement.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = "auto";
+      document.documentElement.style.overflow = "auto";
     }
   }, [menuOpen]);
 
